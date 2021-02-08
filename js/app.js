e2f44de51eb8d9ea3dce7a9733e7e6462c7f6b94
Adam Loksa <adam.loksa@yahoo.com>
2021-02-08 21:35:25 +0100

    Intro video component added.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,32 @@
 // Menu
-// const navBtn = document.querySelector('.nav-toggle');
-// const navigation = document.querySelector('.nav');
-// const hamburgerIcon = document.querySelector('.hamburger');
+const navBtn = document.querySelector('.nav-toggle');
+const navigation = document.querySelector('.nav');
+const hamburgerIcon = document.querySelector('.hamburger');
 
-// navBtn.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     navigation.classList.toggle('nav--open');
-//     hamburgerIcon.classList.toggle('hamburger--open')
-// });
+navBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    navigation.classList.toggle('nav--open');
+    hamburgerIcon.classList.toggle('hamburger--open')
+});
+
 
 
 // Intro page
-const wpIntro = document.querySelectorAll('.wp-intro__animation');
+const wpIntro = document.querySelector('.wp-intro');
+const wpIntroAnimation = document.querySelectorAll('.wp-intro__animation');
 const slideNumber = document.querySelector('.wp-intro__slide--first');
 const skipIntro = document.querySelector('.wp-intro__skip__btn');
 const introVideo = document.querySelector('.wp-intro__video');
 const introProgressBar = document.querySelector('.wp-intro__progress__line');
 
 
-wpIntro.forEach(elem => elem.addEventListener('animationend', animateIntro));
-skipIntro.addEventListener('click', () => console.log('přeskočit intro'));
+wpIntroAnimation.forEach(elem => elem.addEventListener('animationend', animateIntro));
+skipIntro.addEventListener('click', hideIntro);
 introVideo.addEventListener('timeupdate', handleProgress);
 
 function animateIntro(event) {
+    console.log(event);
+
     let currentSlideNumber = slideNumber.innerHTML;
     currentSlideNumber++;
 
@@ -35,18 +39,36 @@ function animateIntro(event) {
             slideNumber.classList.remove('wp-intro__slide--first');
         }, 500);
     }
+
+    if (currentSlideNumber > 4) {
+        setTimeout(function() {
+            hideIntro();
+        }, 500);
+    }
 };
 
 function handleProgress() {
-    // const percent = (introVideo.currentTime / introVideo.duration) * 140;
-    // introProgressBar.style.width = `${percent}%`;
-    introProgressBar.style.width = `100%`;
-    // console.log(introProgressBar);
+    introProgressBar.style = `width: 100%`;
 }
 
-// function progress(){         
-//     introProgressBar.animate({ width: "100%" }, 12000);
-//  };
 
-//  progress();
+function insertIntro() {
+    wpIntro.style = `opacity: 1; display: block`;
+    document.querySelector('body').classList.add('stop-scrolling');
+}
+
+insertIntro();
+
+function hideIntro() {
+    console.log('přeskočit intro');
+    document.querySelector('body').style = 'opacity: 0';
+
+    setTimeout(function() {
+        wpIntro.style = 'display: none;';
+        document.querySelector('body').classList.remove('stop-scrolling');
+        document.querySelector('body').style = 'opacity: 1';
+    }, 500);
+}
+
+
     
