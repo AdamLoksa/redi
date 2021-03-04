693c4090377494944a4013439d02833241a13bbe
Adam Loksa <adam.loksa@yahoo.com>
2021-03-04 21:01:00 +0100

    Mobile notch spacing update.

--- js/app.js
+++ js/app.js
@@ -35,7 +35,9 @@ window.onscroll = function() {
 
 
 // Mobile full height
-const vh = window.innerHeight * 0.01;
+const vh = window.innerHeight;
+const fullScreen = document.querySelector('.page-intro')
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('$h-section', `${vh}px`);
+console.log(vh);
+fullScreen.style.setProperty('height', `${vh}px`);
 
