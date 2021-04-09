7309513fb7292e7f7b1ccbd45579462a3370e9dc
Adam Loksa <adam.loksa@yahoo.com>
2021-04-09 07:16:28 +0200

    Intro video fixes

--- js/intro.js
+++ js/intro.js
@@ -13,11 +13,23 @@ const websiteIntro = (function() {
     skipIntro.addEventListener('click', hideIntro);
     introVideo.addEventListener('timeupdate', handleProgress);
 
+
     function insertIntro() {
         wpIntro.classList.add('wp-intro--show');
         bodyElement.classList.add('stop-scrolling');
         bodyElement.classList.add('wp-intro--fade');
-        // playVideo();
+    }
+
+    introVideo.oncanplay = () => {
+        playVideo();
+    }
+
+    function playVideo() {
+        wpIntroAnimation.forEach(animation => {
+            animation.style = 'animation-name: text-fade;';
+        })
+        
+        slideNumber.style = 'animation-name: text-fade;';
     }
 
     function hideIntro() {
@@ -27,6 +39,7 @@ const websiteIntro = (function() {
             wpIntro.classList.remove('wp-intro--show');
             bodyElement.classList.remove('stop-scrolling');
             bodyElement.style = 'opacity: 1';
+            pageTitleTextAnimation();
         }, 500);
     }
 
@@ -63,14 +76,26 @@ const websiteIntro = (function() {
 if (localStorage.getItem('first-visit')!= 'true') {
     websiteIntro.init();
 } else {
+    pageTitleTextAnimation();
+}
+
+function pageTitleTextAnimation() {
     // Home page title animation
     const pageTitleAnimation = document.querySelectorAll('.page-title__animation--text');
+    const pageTitleAnimationDot = document.querySelectorAll('.page-title__animation--dot');
+
     pageTitleAnimation.forEach(element => {
+        element.style = 'animation-name: title-fade;';
         element.addEventListener('animationend', () => {
             element.style = 'display: none';
         })
+    })   
+
+    pageTitleAnimationDot.forEach(element => {
+        element.style = 'animation-name: dot-fade;';
     })
 }
+
 
 
 
